3ad75fa2fd16cc2afbe52549d7c201b6a35f0def
zzsshalik <43120751+zzsshalik@users.noreply.github.com>
2019-08-12 15:29:30 +0300

    0.1

---
+++ precache-manifest.c293e84efb9e1e3fef6233751132a4d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6d41f1c1b9fe2a43e7dcd4e8ada218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2092be769ecd79c3cc",
+    "url": "/static/css/main.b389dfd6.chunk.css"
+  },
+  {
+    "revision": "99f776ff295a34475bb4",
+    "url": "/static/js/2.01a28b22.chunk.js"
+  },
+  {
+    "revision": "ef2092be769ecd79c3cc",
+    "url": "/static/js/main.89df4eb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
